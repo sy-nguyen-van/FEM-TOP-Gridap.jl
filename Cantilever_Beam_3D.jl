76da576f9f6033cf7d5606415bddbfc2e30297e2
snv22002 <sy.nguyen-van@uconn.edu>
2025-07-07 16:12:11 -0400

    Cantilever_Beam_3D

--- Cantilever_Beam_3D.jl
+++ Cantilever_Beam_3D.jl
@@ -1,8 +1,8 @@
 using Gridap
-L = 100 # Length
-W = 24 # Width
+L = 120 # Length
+W = 24 # Width and Height
 domain = (0, L, 0, W, 0, W) # sizes along X, Y, Z
-partition = (100, 24, 24) # mesh sizes
+partition = (120, 24, 24) # mesh sizes
 model = CartesianDiscreteModel(domain, partition)
 # Tag the left face (x = 0); right face (x = L)
 labels = get_face_labeling(model)
@@ -19,7 +19,7 @@ dΓ = Measure(Γ,degree)
 order = 1
 reffe = ReferenceFE(lagrangian, VectorValue{3,Float64}, order)
 Vₕ = TestFESpace(Ω, reffe; conformity=:H1, dirichlet_tags = "left")
-Uₕ = TrialFESpace(Vₕ) 
+Uₕ = TrialFESpace(Vₕ)
 # Constitutive law for LINEAR ELASTICITY 
 const E = 119e3 # Young's modulus
 const ν = 0.3   # Poisson's ratio
@@ -29,14 +29,23 @@ const μ = E/(2*(1+ν)) # Lamé parameters
 σ(ε₀) = λ * tr(ε₀) * one(ε₀) + 2μ * ε₀ # Stress
 # The weak form
 F(x) = VectorValue(0.0, -1, 0.0) # I want to apply a unit load along Y axis to each node on the right face
-a(u,v) = ∫((σ∘ε₀(u)) ⊙ ε₀(v)) *dΩ # Right-hand size; (∘) Composite functions
-l(v) = ∫(F ⋅ v) * dΓ # Left-hand size
+a(u,v) = ∫((σ∘ε₀(u)) ⊙ ε₀(v)) *dΩ # Left-hand size; (∘) Composite functions
+l(v) = ∫(F ⋅ v) * dΓ # Right-hand size
 # Solution of the FE problem
 op = AffineFEOperator(a,l,Uₕ,Vₕ)
-uₕ = solve(op)
-# Export the displacement
-writevtk(Ω,"3D_Problem_uh",cellfields=["u"=>uₕ])
+uh = solve(op)
+# Export the displacement, strain, stress
+writevtk(Ω,"Cantilever_Beam_3D_uh",cellfields=["uh"=>uh,"epsi"=>ε₀(uh),"sigma"=>σ∘ε₀(uh)])
 
-
+# # Compute the compliance 
+# num_ele = num_cells(Ω) # No of elements
+# c = 0 # compliance
+# K = ... # Global stiffness matrix
+# for e=1:num_ele
+#     dof_e == .... # degree of freedom of element "e"
+#     ue = uh(dof_e) # the element displacement vector
+#     ke = K(dof_e,dof_e) # the element stiffness matrix
+#     c = c+ transpose(ue)*ke*ue # update compliance
+# end
 
 
